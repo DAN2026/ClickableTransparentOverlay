3fadd5f037c81e914a6bf2f3033986cdb74289b6
DAN2026 <danuniversity.education@outlook.com>
2025-03-07 20:01:10 +0000

    Update SampleOverlay.cs

--- Examples/SimpleExample/SampleOverlay.cs
+++ Examples/SimpleExample/SampleOverlay.cs
@@ -6,37 +6,23 @@
 
     internal class SampleOverlay : Overlay
     {
-        private bool wantKeepDemoWindow = true;
         private int FPSHelper;
 
-        public SampleOverlay() : base(3840, 2160)
+        public SampleOverlay() : base(3840, 2160) // Constructor
         {
-            this.FPSHelper = this.FPSLimit;
         }
 
-        protected override Task PostInitialized()
-        {
-            return Task.CompletedTask;
-        }
+
 
         protected override void Render()
         {
-            ImGui.ShowDemoWindow(ref wantKeepDemoWindow);
 
             if (ImGui.Begin("FPS Changer"))
             {
-                if (ImGui.InputInt("Set FPS", ref FPSHelper))
-                {
-                    this.FPSLimit = this.FPSHelper;
-                }
-            }
 
+            }
             ImGui.End();
 
-            if (!this.wantKeepDemoWindow)
-            {
-                this.Close();
-            }
         }
     }
 }
